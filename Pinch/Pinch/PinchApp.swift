63ecf4dc779d6eada2c5fe5289ad48a0674d2a25
SebiC98 <sebastiancioata98@gmail.com>
2023-01-14 15:47:49 +0200

    Base App

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Sebastian Cioată on 14.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
